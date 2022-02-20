ff24b68e66516d8e6534e2d6550a119b5853bb8b
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-02-20 15:11:42 +0430

    set style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row, Dropdown } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import { Icon } from '@iconify/react';
+const languages = [
+  {
+    code: "en",
+    name: "English",
+    country_code: "gb",
+  },
+  {
+    code: "af",
+    name: "فارسی",
+    country_code: "af",
+  },
+];
 
 function App() {
   const { t } = useTranslation();
-  return <Container>{t("welcome_message")}</Container>;
+  return (
+    <Container>
+      <Row className="pt-1">
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle id="dropdown-basic">
+            <Icon icon="ant-design:global-outlined" />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              {languages.map(lan=>(
+                <Dropdown.Item key={lan.code} >{lan.name}</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+     <h1 className="text-center">{t('welcome_message')}</h1>
+    </Container>
+  );
 }
 
 export default App;
